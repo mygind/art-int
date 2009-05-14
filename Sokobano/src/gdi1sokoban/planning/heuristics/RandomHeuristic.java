7c2382068f12f81b99739134a01b46a6fa7e66d7
Brian Stengaard <brian136@pop.k-net.dk>
2009-05-15 01:25:26 +0200

    Wuhu

---
+++ Sokobano/src/gdi1sokoban/planning/heuristics/RandomHeuristic.java
@@ -0,0 +1,26 @@
+package gdi1sokoban.planning.heuristics;
+
+import gdi1sokoban.planning.Board;
+
+import java.util.Random;
+
+public class RandomHeuristic extends Heuristic {
+
+    private static final int variance = 10000;
+
+    private Random r;
+	public RandomHeuristic(Board board) {
+		super(board);
+		r = new Random();
+	}
+	
+	@Override
+	public int estimate(Board b) {
+	    return r.nextInt(10000);
+	}
+	
+	@Override
+	public String toString() {
+		return "RandomHeuristic";
+	}
+}
